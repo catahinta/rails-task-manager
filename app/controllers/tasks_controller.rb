22fd50453ce7a94e30c5a4eeae0815a245a03f72
Kata Nagy <nagykata98@gmail.com>
2023-11-14 15:00:43 +0000

    Refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,24 +18,25 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to index_path, notice: 'Task was successfully deleted.', status: :see_other
+    redirect_to tasks_path, notice: 'Task was successfully deleted.', status: :see_other
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
